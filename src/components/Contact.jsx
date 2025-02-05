c1c4426c1ceafc0db1cd5afb78a876049fa48417
Nitin Singh <borntocode.ea@gmail.com>
2025-02-05 22:59:39 +0530

    bug fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@
 
 const socialLinks = [
   {
-    href: "https://www.github.com/codewithsadee-org",
+    href: "https://www.github.com/code-ea",
     icon: (
       <svg
         width="24"
@@ -23,7 +23,7 @@ const socialLinks = [
     alt: "GitHub",
   },
   {
-    href: "https://www.linkedin.com/in/codewithsadee",
+    href: "https://www.linkedin.com/in/ncodes",
     icon: (
       <svg
         width="24"
@@ -41,7 +41,7 @@ const socialLinks = [
     alt: "LinkedIn",
   },
   {
-    href: "https://x.com/codewithsadee_",
+    href: "https://x.com/paarthea",
     icon: (
       <svg
         width="24"
@@ -59,7 +59,7 @@ const socialLinks = [
     alt: "Twitter X",
   },
   {
-    href: "https://www.instagram.com/codewithsadee",
+    href: "https://www.instagram.com/nitinorwho",
     icon: (
       <svg
         width="25"
